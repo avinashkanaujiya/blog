fe64cba7e983016e67e43223089f25fc464083ef
avinashkanaujiya <avinashkanaujiya@users.noreply.github.com>
2022-05-15 12:22:30 +0000

    deploy: c535165295c7b9117faa74cbed4a6d87845d0b5d

---
+++ _nuxt/static/1652617347/moneylife-newsletter/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652617347",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmoneylife-newsletter",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d7a85b5d"}}}}(null));
