8c83e7d1567526f3832606009a9de1a7ffdfe3a5
avinashkanaujiya <avinashkanaujiya@users.noreply.github.com>
2022-05-16 16:03:24 +0000

    deploy: 127dc7f1dd167b976822f8582f918f0fdb6469af

---
+++ _nuxt/static/1652717003/mongodb/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652717003",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmongodb",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8e348a7d"}}}}(null));
