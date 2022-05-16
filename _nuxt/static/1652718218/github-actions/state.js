a65ea8af5b7cdb84a6d9c3ca31e520713634e09c
avinashkanaujiya <avinashkanaujiya@users.noreply.github.com>
2022-05-16 16:24:29 +0000

    deploy: f86255a509ab8e57f759870fb393596d46698e3c

--- _nuxt/static/1652718218/github-actions/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652718218",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgithub-actions",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"74b965f3"}}}}(null));
