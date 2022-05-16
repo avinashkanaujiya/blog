660e438b2e54bbe997e47da94204f8551d100d70
avinashkanaujiya <avinashkanaujiya@users.noreply.github.com>
2022-05-16 16:05:07 +0000

    deploy: 3bc5a336cd9b2305bb8bf865aaa51371ba82a2ed

--- _nuxt/static/1652717003/reading-in-progress/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652717003",layout:"default",error:a,serverRendered:true,routePath:"\u002Freading-in-progress",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8e348a7d"}}}}(null));
