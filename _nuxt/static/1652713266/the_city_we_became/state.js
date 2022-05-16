7abe3d01411e0dc14985ca43afcfd46a2792e03e
avinashkanaujiya <avinashkanaujiya@users.noreply.github.com>
2022-05-16 15:21:44 +0000

    deploy: 487b7da3c40453fc0194bdc40ddd822c6b103a59

--- _nuxt/static/1652713266/the_city_we_became/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652713266",layout:"default",error:a,serverRendered:true,routePath:"\u002Fthe_city_we_became",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d8eae274"}}}}(null));
