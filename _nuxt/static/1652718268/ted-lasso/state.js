e236d2878db4f96e7dd96b14bd6b7f45c4ea44d7
avinashkanaujiya <avinashkanaujiya@users.noreply.github.com>
2022-05-25 14:31:46 +0000

    deploy: 001a78454e1615520bc1aeb685d9a3b26d623b74

--- _nuxt/static/1652718268/ted-lasso/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652718268",layout:"default",error:a,serverRendered:true,routePath:"\u002Fted-lasso",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3c593afe"}}}}(null));
