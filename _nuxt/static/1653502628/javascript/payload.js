4c3cf977b08a734c3a453707ddb20c58be7d98a8
avinashkanaujiya <avinashkanaujiya@users.noreply.github.com>
2022-05-26 14:29:50 +0000

    deploy: 030e80b26167d9fa1f429bd16180d76d3e35b5c7

--- _nuxt/static/1653502628/javascript/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/javascript", {data:[{article:{slug:"javascript",title:"JavaScript",updatedAt:"2022-05-15T00:00:00.000Z",toc:[],body:{type:"root",children:[]},dir:"\u002Farticles",path:"\u002Farticles\u002Fjavascript",extension:".md",createdAt:"2022-05-25T18:16:41.365Z"}}],fetch:{},mutations:void 0});
--- _nuxt/static/1653502628/vue-framework/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/vue-framework", {data:[{article:{slug:"vue-framework",title:"Vue",updatedAt:"2022-05-15T00:00:00.000Z",toc:[],body:{type:"root",children:[]},dir:"\u002Farticles",path:"\u002Farticles\u002Fvue-framework",extension:".md",createdAt:"2022-05-25T18:16:41.365Z"}}],fetch:{},mutations:void 0});
