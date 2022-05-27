e9f4909db9ed869db444ea287c7e7b842bc27379
avinashkanaujiya <avinashkanaujiya@users.noreply.github.com>
2022-05-27 20:57:54 +0000

    deploy: c66aaf9ff0d8e8b1d0ab8113232240165d56f1b2

--- _nuxt/static/1653684423/git/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653684423",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgit",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"38dd3d57"}}}}(null));
