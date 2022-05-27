371a72ce5425cb25d1ed090e0cb0d37ca6ae84a1
avinashkanaujiya <avinashkanaujiya@users.noreply.github.com>
2022-05-27 20:23:25 +0000

    deploy: 28f75dccab368c94e9342851c3fd2a8d92707bec

--- _nuxt/static/1653680461/overlord-review/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653680461",layout:"default",error:a,serverRendered:true,routePath:"\u002Foverlord-review",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c6c00e69"}}}}(null));
