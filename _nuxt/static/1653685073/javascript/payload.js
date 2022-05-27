0a582b51567da25eea92fcd7328f672d7681126b
avinashkanaujiya <avinashkanaujiya@users.noreply.github.com>
2022-05-27 21:01:01 +0000

    deploy: 3beb2be9d542ac251114500abc359c5f0cb02dc6

--- _nuxt/static/1653685073/javascript/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/javascript", {data:[{article:{slug:"javascript",title:"JavaScript",updatedAt:"2022-05-15T00:00:00.000Z",toc:[],body:{type:"root",children:[]},dir:"\u002Farticles",path:"\u002Farticles\u002Fjavascript",extension:".md",createdAt:"2022-05-27T20:57:25.699Z"}}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1653685260/vue-framework/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653685260",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvue-framework",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5973e15a"}}}}(null));
