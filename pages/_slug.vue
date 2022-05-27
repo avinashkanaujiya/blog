3beb2be9d542ac251114500abc359c5f0cb02dc6
avinashkanaujiya <github@avinashkanaujiya.me>
2022-05-28 02:30:16 +0530

    Update _slug.vue

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -33,7 +33,7 @@ body{
   font-size: 14px;
   // text-align: justify;
   padding: 10px;
-  line-height: 1.2rem;
+  line-height: 1.4rem;
   .nuxt-content {
     // ul{
     //   margin: 15px 0 15px 60px ;
